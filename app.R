8c4cfb46f71bb2256f3227b4ae144e832b80c9e2
mattfishaus <matt@firststep.co>
2019-12-05 10:02:00 +1100

    Adding ability to write table to Postgres

--- app.R
+++ app.R
@@ -3,13 +3,17 @@ library(shiny)
 library(DT)
 
 #connect to Postgres db
-con <- dbConnect(RPostgres::Postgres(),dbname = 'roster_scores', host = 'db-postgresql-sgp1-84573-do-user-1917838-0.db.ondigitalocean.com', port = 25060, user = 'doadmin', password = 'lzyhz8inuf02262q')
+conn <- dbConnect(RPostgres::Postgres(),dbname = 'roster_scores', host = 'db-postgresql-sgp1-84573-do-user-1917838-0.db.ondigitalocean.com', port = 25060, user = 'doadmin', password = 'lzyhz8inuf02262q')
 
 #query the db and save data frame
-score_players <- dbGetQuery(con, "SELECT * from player_scores")
+player_scores <- dbGetQuery(conn, "SELECT * from player_scores")
 
 #create new data frame by team
-score_team <- score_players %>% select(weekno, teamno, fgm, fga, ftm, fta, tpm, pts, reb, ast, stl, blk, tover) %>% group_by(weekno, teamno) %>% summarise(fgPct= round(sum(fgm)/sum(fga),4), ftPct= round(sum(ftm)/sum(fta),4), tpm= sum(tpm), pts= sum(pts), reb= sum(reb), ast= sum(ast), stl= sum(stl), blk= sum(blk), tover= sum(tover))
+team_scores <- player_scores %>% select(weekno, teamno, fgm, fga, ftm, fta, tpm, pts, reb, ast, stl, blk, tover) %>% group_by(weekno, teamno) %>% summarise(fgPct= round(sum(fgm)/sum(fga),4), ftPct= round(sum(ftm)/sum(fta),4), tpm= sum(tpm), pts= sum(pts), reb= sum(reb), ast= sum(ast), stl= sum(stl), blk= sum(blk), tover= sum(tover))
+
+#write data frame by team into Postgres
+dbRemoveTable(conn,"team_scores")
+dbWriteTable(conn,"INSERT INTO team_scores",team_scores)
 
 #create shinyapp
 ui <- fluidPage(
@@ -17,10 +21,10 @@ ui <- fluidPage(
   fluidRow(
            selectInput("week", "Week:",
                        c("All",
-                         unique(as.character(score_team$weekno)))),
+                         unique(as.character(team_scores$weekno)))),
            selectInput("team", "Team:",
                        c("All",
-                         unique(as.character(score_team$teamno))))
+                         unique(as.character(team_scores$teamno))))
   ),
   # Create a new row for the table.
   DT::dataTableOutput("result")
@@ -29,7 +33,7 @@ ui <- fluidPage(
 server <- function(input, output) {
   # Filter data based on selections
   output$result <- DT::renderDataTable(DT::datatable({
-    data <- score_team
+    data <- team_scores
     if (input$week != "All") {
       data <- data[data$weekno == input$week,]
     }
