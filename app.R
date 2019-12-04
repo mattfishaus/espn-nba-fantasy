b1e62d768fc9b85543184c9da1b217cb94b6c9da
mattfishaus <matt@firststep.co>
2019-12-05 10:34:13 +1100

    write team_scores to db

--- app.R
+++ app.R
@@ -4,17 +4,17 @@ library(dplyr)
 library(DT)
 
 #connect to Postgres db
-con <- dbConnect(RPostgres::Postgres(),dbname = 'roster_scores', host = 'db-postgresql-sgp1-84573-do-user-1917838-0.db.ondigitalocean.com', port = 25060, user = 'doadmin', password = 'lzyhz8inuf02262q')
+conn <- dbConnect(RPostgres::Postgres(),dbname = 'roster_scores', host = 'db-postgresql-sgp1-84573-do-user-1917838-0.db.ondigitalocean.com', port = 25060, user = 'doadmin', password = 'lzyhz8inuf02262q')
 
 #query the db and save data frame
-player_scores <- dbGetQuery(con, "SELECT * from player_scores")
+player_scores <- dbGetQuery(conn, "SELECT * from player_scores")
 
 #create new data frame by team
 team_scores <- player_scores %>% select(weekno, teamno, fgm, fga, ftm, fta, tpm, pts, reb, ast, stl, blk, tover) %>% group_by(weekno, teamno) %>% summarise(fgPct= round(sum(fgm)/sum(fga),4), ftPct= round(sum(ftm)/sum(fta),4), tpm= sum(tpm), pts= sum(pts), reb= sum(reb), ast= sum(ast), stl= sum(stl), blk= sum(blk), tover= sum(tover))
 
 #write data frame by team into Postgres
-#dbRemoveTable(con,"team_scores")
-#dbWriteTable(con,"INSERT INTO team_scores",team_scores)
+dbRemoveTable(conn,"team_scores")
+dbWriteTable(conn,"INSERT INTO team_scores",team_scores)
 
 #create shinyapp
 ui <- fluidPage(
