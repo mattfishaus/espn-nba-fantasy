d5d0eeba18eaee2726817fa9c1d0131924ad0727
mattfishaus <matt@firststep.co>
2019-12-04 22:37:25 +1100

    New data

--- app.R
+++ app.R
@@ -1,19 +1,44 @@
 library(RPostgres)
 library(shiny)
+library(tidyverse)
+library(DT)
 
-#Connect to Postgres db
+#connect to Postgres db
 con <- dbConnect(RPostgres::Postgres(),dbname = 'roster_scores', host = 'db-postgresql-sgp1-84573-do-user-1917838-0.db.ondigitalocean.com', port = 25060, user = 'doadmin', password = 'lzyhz8inuf02262q')
 
-#Query the db and save data frame
-score_players <- dbGetQuery(con, "SELECT * from team_scores")
+#query the db and save data frame
+score_players <- dbGetQuery(con, "SELECT * from player_scores")
 
+#create new data frame by team
+score_team <- score_players %>% select(weekno, teamno, fgm, fga, ftm, fta, tpm, pts, reb, ast, stl, blk, tover) %>% group_by(weekno, teamno) %>% summarise(fgPct= round(sum(fgm)/sum(fga),4), ftPct= round(sum(ftm)/sum(fta),4), tpm= sum(tpm), pts= sum(pts), reb= sum(reb), ast= sum(ast), stl= sum(stl), blk= sum(blk), tover= sum(tover))
+
+#create shinyapp
 ui <- fluidPage(
-  titlePanel("The League Power Rankings"),
-  tableOutput("table")
+  h2("The League"),
+  fluidRow(
+           selectInput("week", "Week:",
+                       c("All",
+                         unique(as.character(score_team$weekno)))),
+           selectInput("team", "Team:",
+                       c("All",
+                         unique(as.character(score_team$teamno))))
+  ),
+  # Create a new row for the table.
+  DT::dataTableOutput("result")
 )
 
 server <- function(input, output) {
-  output$table <- renderTable(score_players)
+  # Filter data based on selections
+  output$result <- DT::renderDataTable(DT::dataTable({
+    data <- score_team
+    if (input$man != "All") {
+      data <- data[data$weekno == input$week,]
+    }
+    if (input$trans != "All") {
+      data <- data[data$teamno == input$team,]
+    }
+    data
+  }))
 }
 
 shinyApp(ui = ui, server = server)
