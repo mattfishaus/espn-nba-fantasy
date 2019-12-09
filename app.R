b287812b51937b3735e10b54b5a0a14a109b7d2f
mattfishaus <matt@firststep.co>
2019-12-09 13:35:52 +1100

    fix column width

--- app.R
+++ app.R
@@ -64,7 +64,7 @@ server <- function(input, output) {
       data <- data[data$team == input$team,]
     }
     data
-  },options = list(autoWidth = TRUE, pageLength = 11)))
+  },options = list(autoWidth = TRUE, columnDefs = list(list(width = '50px', targets = "_all")), pageLength = 11)))
 }
 
 shinyApp(ui = ui, server = server)
