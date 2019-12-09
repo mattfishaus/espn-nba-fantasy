4157680f851e85a37c51603416142f021661d690
mattfishaus <matt@firststep.co>
2019-12-09 16:14:14 +1100

    fix summarise

--- app.R
+++ app.R
@@ -38,7 +38,7 @@ funLOSS <- function(x) {
 matchup_results <- matchup_results %>% 
   select(weekno, team, opponent, fgpct, ftpct, tpm, pts, reb, ast, stl, blk, tover) %>%
   group_by(weekno, team, opponent) %>%
-  summarise(WINS = sum(funWIN(fgpct)) + sum(funWIN(ftpct)) + sum(funWIN(tpm)) + sum(fuWIN(pts)) + sum(funWIN(ast)) + sum(funWIN(stl)) + sum(funWIN(blk)) + sum(funWIN(tover)))
+  summarise(WINS = funWIN(fgpct) + funWIN(ftpct) + funWIN(tpm) + fuWIN(pts) + funWIN(ast) + funWIN(stl) + funWIN(blk) + funWIN(tover))
 
 #create shinyapp
 ui <- fluidPage(
