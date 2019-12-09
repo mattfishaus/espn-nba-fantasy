d1cdd2af9cb114b5179e0890c4f275a740c83d4e
mattfishaus <matt@firststep.co>
2019-12-09 11:26:28 +1100

    Correcting

--- app.R
+++ app.R
@@ -53,8 +53,7 @@ server <- function(input, output) {
       data <- data[data$team == input$team,]
     }
     data
-  }
-  ),options = list(pageLength = 25))
+  },options = list(pageLength = 25)))
 }
 
 shinyApp(ui = ui, server = server)
